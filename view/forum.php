ee6d332ad8f8af93bfdb3a5a219994ee24cc3acf
ankit sharma <as4808385@gmail.com>
2023-05-18 11:32:19 +0530

    changed file names to .php changed style of page

--- view/forum.php
+++ view/forum.php
@@ -5,10 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ankit Sharma | Forum</title>
+    <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
-    <form action="../db/data.php" method="post">
-        <table>
+    <div class="container">
+        <div class="home"><a href="../index.php">Home</a></div>
+    </div>
+    <!-- <form action="../db/data.php" method="post"> -->
+        <!-- <table>
             <tr>
                 <th>Type your query</th>
                 <td><textarea name="message" id="message" cols="30" rows="10"></textarea></td>
@@ -20,7 +24,7 @@
             <tr>
                 <td><input type="submit" value="submit"></td>
             </tr>
-        </table>
-    </form>
+        </table> -->
+    <!-- </form> -->
 </body>
 </html>
--- view/skills.php
+++ view/skills.php
@@ -5,11 +5,11 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Ankit Sharma | Skills</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
 	<div class="container">
-		<div class="home"><a href="index.html">Home</a></div>
+		<div class="home"><a href="../index.php">Home</a></div>
 		<div class="header">
 			<div class="title"><h1>Web Development</h1></div>
 			<div class="sub-menu">
